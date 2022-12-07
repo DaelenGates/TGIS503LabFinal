2908efd67dcd369825c6c951e8d6ca485cfe1f21
DaelenGates <115597283+DaelenGates@users.noreply.github.com>
2022-12-06 20:29:14 -0800

    Big push
    I still need to put conditionals for the Labeling so people know what should be done or shouldnt be done  depending on the arseic or lead levels. Also perhaps update the CSS.

--- scripts.js
+++ scripts.js
@@ -18,45 +18,105 @@ var leaf = L.icon({
       // shadowAnchor: [4, 62],  // the same for the shadow
       // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
     });
-// Adding geoJson file?
+
+
+    // this adds arsenic projections onto map1 changing color depending on the predicted arsenic ppms
+    var nam = 1;
+ var arse = L.geoJson(ars,{
+      interactive: false,
+      PointToLayer: function(feature, latlng){
+        var nam = (feature.properties.NAME);
+        nam.bindPopup("hi");
+        console.log('hihi');
+        return nam;
+      },
+      // marker.bindPopup('hi');
+      style: function(feature){
+        if(feature.properties.NAME == "20 ppm to 40 ppm"){
+          return{"color": '#fecc5c'};
+        }
+        if(feature.properties.NAME == "40.1 ppm to 100 ppm"){
+          return{"color": '#fd8d3c'};
+        }
+        if(feature.properties.NAME == "Over 100 ppm"){
+          return{"color": '#bd0026'};
+        }
+        if(feature.properties.NAME == "Under 20 ppm"){
+          return{"color": '#74a9cf'}
+        }
+        else{
+          return{"color": "grey"};
+        }
+      }
+    }).addTo(map);
 
 // adding garden maps as a variable so they can be split up by the clustering plugin
 var gardens = L.geoJson(gar, {
+    //   // primary way of showing pop up on garden
       pointToLayer: function(feature, latlng){
       var marker = L.marker(latlng, {icon: leaf});
-      // marker.bindPopup("Status: " + feature.properties.Status + "<br>Capacity:" + feature.properties.Capacity);
+      marker.bindPopup("Garden name: " + feature.properties.Garden_Nam + '<br>' + "Asenic Levels: " + feature.properties.NAME);
+      console.log(nam);
       return marker;
     }
+    // second way of adding popup
+    // onEachFeature: function(f, l){
+    //   l.bindPopup(gar.f.properties.Garden_Nam);
+    // }
+
 });
+
 
 // this adds the clustering version of gardens to map1
 var clusters = L.markerClusterGroup({showCoverageOnHover: false});
 clusters.addLayer(gardens);
+// clusters.addLayer(arse);
 map.addLayer(clusters);
 
-// this adds arsenic projections onto map1 changing color depending on the predicted arsenic ppms
-L.geoJson(ars,{
-  interactive: false,
-  style: function(feature){
-    if(feature.properties.NAME == "20 ppm to 40 ppm"){
-      return{"color": '#fecc5c'};
-    }
-    if(feature.properties.NAME == "40.1 ppm to 100 ppm"){
-      return{"color": '#fd8d3c'};
-    }
-    if(feature.properties.NAME == "Over 100 ppm"){
-      return{"color": '#bd0026'};
-    }
-    if(feature.properties.NAME == "Under 20 ppm"){
-      return{"color": '#74a9cf'}
-    }
-    else{
-      return{"color": "grey"};
-    }
-  }
-}).addTo(map);
+// trying to get popup to show informatino from two layers using featureGroup !!! FAILD
+// var group = L.featureGroup([clusters, arse], {
+//   intaractive: true,
+//   onEachFeature: function(f, l){
+//     l.bindPopup(f.properties.Garden_Nam);
+//     console.log('hi');
+//
+//   }
+// })
 
-// This adds the legend to map1
+  // .bindPopup('clusters.feature.properties.Garden_Nam')
+  // .on('click', function(feature) { alert('Clicked on a group!'); })
+  // .addTo(map);
+
+// attempt to display feature data using .on('click') method !!! FAILD
+// arse.on('click', function(e, feature, arse) {
+//   L.geoJson(ars,{
+//        pointToLayer: function (feature, latlong){
+//          var sam = ('feature.properties.NAME');
+//          return sam;
+//        }
+//
+//      })
+//      var ptcoords = e.latlng;
+//      // var sam = e.arse.features.properties.NAME;
+//      L.popup()
+//         // .setContent(container)
+//         .setLatLng(e.latlng)
+//         .openOn(map);
+//     console.log(e.latlng),
+//     console.log(ptcoords);
+//
+//   })
+
+
+    // console.log(feature.properties.Garden_Nam),
+    // console.log(gar.properties.Garden_Nam);
+
+    // map.eachLayer(function(ars){
+    //     // console.log(arse.properties.NAME);
+    // })
+  // });
+
+// This adds the legends to map1
 var Legend = L.control.legend({
   position:"bottomright",
   opacity: 0.6,
@@ -70,6 +130,51 @@ var Legend = L.control.legend({
   ]
 }).addTo(map);
 
+// Legend for the Asenic part of the map
+var Legend2 = L.control.legend({
+  title: "Levels of Arsenic (PPM)",
+  position:"bottomleft",
+  opacity: 0.6,
+  legends:[
+    {
+      label:"Under 20 PPM",
+      type: "rectangle",
+      color: '#74a9cf',
+      fillOpacity: "0.8",
+      weight: 4
+    },
+    {
+      label:"20 to 40 PPM",
+      type: "rectangle",
+      color: '#fecc5c',
+      fillOpacity: "0.8",
+      weight: 4
+    },
+    {
+      label:"40.1 to 100 PPm",
+      type: "rectangle",
+      color: '#fd8d3c',
+      fillOpacity: "0.8",
+      weight: 4
+    },
+    {
+      label:"Over 100 PPM",
+      type: "rectangle",
+      color: '#bd0026',
+      fillOpacity: "0.8",
+      weight: 4
+    },
+    {
+      label:"Unknown or undefined PPM",
+      type: "rectangle",
+      color: 'grey',
+      fillOpacity: "0.8",
+      weight: 4
+    },
+  ]
+}).addTo(map);
+
+
 // JS for map2
 
 var map2 = L.map('map2').setView([47.2562,-122.4582],12);
@@ -82,11 +187,14 @@ var map2 = L.map('map2').setView([47.2562,-122.4582],12);
 }).addTo(map2);
 // adding garden maps as a variable so they can be split up by the clustering plugin to map 2
 var gardens = L.geoJson(gar, {
-      pointToLayer: function(feature, latlng){
+    pointToLayer: function(feature, latlng){
       var marker = L.marker(latlng, {icon: leaf});
-      // marker.bindPopup("Status: " + feature.properties.Status + "<br>Capacity:" + feature.properties.Capacity);
+      var LdRsk = feature.properties.LdRsk;
+      var LdRskR = Math.round(LdRsk);
+      marker.bindPopup("Garden name: " + feature.properties.Garden_Nam + '<br>' + "Percent chance of heavy lead risk: " + LdRskR + "%");
       return marker;
-    }
+      }
+
 });
 
 // this adds the clustering version of gardens to map2
@@ -130,5 +238,48 @@ var Legend = L.control.legend({
       url: "garden.png",
       fillOpacity: "0.8"
     }
+  ]
+}).addTo(map2);
+
+var Legend2 = L.control.legend({
+  title: "Percent chance of there being significant Lead levels",
+  position:"bottomleft",
+  opacity: 0.6,
+  legends:[
+    {
+      label:"Under 31%",
+      type: "rectangle",
+      color: '#ffffb2',
+      fillOpacity: "0.8",
+      weight: 4
+    },
+    {
+      label:"31% <= 57%",
+      type: "rectangle",
+      color: '#fecc5c',
+      fillOpacity: "0.8",
+      weight: 4
+    },
+    {
+      label:"57% <= 77%",
+      type: "rectangle",
+      color: '#fd8d3c',
+      fillOpacity: "0.8",
+      weight: 4
+    },
+    {
+      label:"77% <= 90%",
+      type: "rectangle",
+      color: '#f03b20',
+      fillOpacity: "0.8",
+      weight: 4
+    },
+    {
+      label:"90% <= 100%",
+      type: "rectangle",
+      color: '#bd0026',
+      fillOpacity: "0.8",
+      weight: 4
+    },
   ]
 }).addTo(map2);
